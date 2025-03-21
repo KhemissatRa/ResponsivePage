4782e454b33045e929e168a879d2e1c2692f4cb1
khemissat <khemissat.ra@gmail.com>
2025-03-21 04:30:28 +0100

    trying to fix image for deploy

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,5 +1,6 @@
 import Featears from './featears'
 import userTumb from '../assets/User Thumb (1).png'
+import mobile from '../assets/Mobile-App-Placeholder-2 (1).png'
 export default function Testimonials() {
   return (
 <div className='bg-[#581C87] flex flex-col md:flex-row justify-center md:justify-betwen  items-center px-8 py-4 '>
@@ -8,10 +9,10 @@ export default function Testimonials() {
     styleTwo="text-5xl"
     styleOne="w-full h-full"
     styleThree="w-full h-full"
-      src="src\assets\Mobile-App-Placeholder-2 (1).png"
+      src={mobile}
       title="Bigapp got to the next level"
       upTitle="Testimonials" 
-    />" 
+    />
     </div>
 <div className='text-white space-y-4'>
     <p>“Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque faucibus.”</p>
